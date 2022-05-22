eb66dd6f31c6b2311a08d9103d57e747b98a9a3b
Ptrnd <putranda.bagus@gmail.com>
2022-05-22 08:13:25 +0700

    ganti route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('profil', function () { 
 //     return view('profil'); 
 // });
-Route::get('/coba', function () {
-    return view('coba');
+Route::get('/cobas', function () {
+    return view('cobas');
 });
